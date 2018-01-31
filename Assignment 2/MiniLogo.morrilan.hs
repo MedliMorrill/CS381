e23994175228d706ed7a5d4f980ad01267090ae6
Megadrew <morrilan@oregonstate.edu>
2018-01-31 15:28:07 -0800

    Finished Task 5: adding macros and stripName Haskell functions, added comments

--- Assignment 2/MiniLogo.morrilan.hs
+++ Assignment 2/MiniLogo.morrilan.hs
@@ -11,7 +11,10 @@ module MiniLogo where
 
 import Prelude hiding (Num)
 
+
+
 -- *** Task 1 *** --
+
 type Num   = Int
 type Var   = [Char]
 type Macro = String
@@ -32,6 +35,7 @@ data Cmd  = Pen Mode
           | Define Macro [Var] Prog
           | Call Macro [Expr]
   deriving (Eq,Show)
+
 
 -- *** Task 2 *** --
 
@@ -60,17 +64,38 @@ nix = Define "nix" ["x","y","w","h"] [Call "line" [Ref "x", Ref "y", Add (Ref "x
 
 
 
-
 -- *** Task 4 *** --
 
--- Iterates n recursively, till it gets to 0, then adds a call and returns it back as a prog
---  added rec
+-- Draws from origin up one point then right, repeating it n times. 
+--  Iterates n recursively, till it gets to 0, then adds a call and returns it back as a Prog
+--  (added comments for variable check)     x1         y1         x2        y2                      x1         y1       x2     y2
 
 steps :: Int -> Prog
-steps 0 = []                         --      x1         y1         x2        y2                      x1         y1       x2     y2
-steps n =  steps (n-1) ++ [Call "line" [Lit (n-1), Lit (n-1), Lit (n-1), Lit (n)], Call "line" [Lit (n-1), Lit (n-1), Lit n, Lit n]]
+steps 0 = []
+steps n = steps (n-1) ++ [Call "line" [Lit (n-1), Lit (n-1), Lit (n-1), Lit (n)], Call "line" [Lit (n-1), Lit (n-1), Lit n, Lit n]]
 
 -- steps :: Int -> Cmd
 -- steps 0 = Call "line" [Lit 0, Lit 0, Lit 0, Lit 0]
 -- steps n = Define "steps" ["x","y"] (iterateSteps n)
 
+
+
+-- *** Task 5 *** --
+
+-- prog to list of Cmds, then list of Cmds to list of names of Macros
+-- macros recursively calls itself, through the list of Cmds entered, 
+--  sends each one to stripName and returns the Macro names as a list
+
+macros :: Prog -> [Macro]
+macros [] = []
+macros (x : xs) = (macros xs) ++ [stripName x]
+
+-- takes Cmd from macros and strips the Macro name from the Call and Define
+
+stripName :: Cmd -> Macro
+stripName (Define m v p) = m
+stripName (Call m e)     = m
+
+
+
+-- *** Task 6 *** --
