c803d42c1ca75c7a6e3652be187c45da58912b77
Megadrew <morrilan@oregonstate.edu>
2018-01-31 16:57:12 -0800

    Task 6 Started, Temp Help group of comments added, added a single test case

--- Assignment 2/MiniLogo.morrilan.hs
+++ Assignment 2/MiniLogo.morrilan.hs
@@ -13,6 +13,7 @@ import Prelude hiding (Num)
 
 
 
+
 -- *** Task 1 *** --
 
 type Num   = Int
@@ -37,8 +38,11 @@ data Cmd  = Pen Mode
   deriving (Eq,Show)
 
 
+
+
 -- *** Task 2 *** --
 
+--
 -- define line(x1,y1,x2,y2) {
 --   pen up; move(x1,y1);
 --   pen down; move(x2,y2);
@@ -50,8 +54,10 @@ line = Define "line" ["x1","y1", "x2", "y2"] [Pen Up, Move (Ref "x1") (Ref "y1")
 
 
 
+
 -- *** Task 3 *** --
 
+-- 
 -- define nix (x,y,w,h) {
 --   line(x,y,x+w,y+h);
 --   line(x+w,y,x,y+h);
@@ -61,6 +67,7 @@ line = Define "line" ["x1","y1", "x2", "y2"] [Pen Up, Move (Ref "x1") (Ref "y1")
 --  second line starts from the bottom right to the top left of the X
 
 nix = Define "nix" ["x","y","w","h"] [Call "line" [Ref "x", Ref "y", Add (Ref "x") (Ref "w"), Add (Ref "y") (Ref "h")], Call "line" [Add (Ref "x") (Ref "w"), Ref "y", Ref "x", Add (Ref "y") (Ref "h")]] 
+
 
 
 
@@ -74,9 +81,6 @@ steps :: Int -> Prog
 steps 0 = []
 steps n = steps (n-1) ++ [Call "line" [Lit (n-1), Lit (n-1), Lit (n-1), Lit (n)], Call "line" [Lit (n-1), Lit (n-1), Lit n, Lit n]]
 
--- steps :: Int -> Cmd
--- steps 0 = Call "line" [Lit 0, Lit 0, Lit 0, Lit 0]
--- steps n = Define "steps" ["x","y"] (iterateSteps n)
 
 
 
@@ -98,4 +102,69 @@ stripName (Call m e)     = m
 
 
 
+
 -- *** Task 6 *** --
+
+pretty :: Prog -> String
+pretty [] = []
+pretty (x : xs) = pretty xs ++ stripAll x
+
+stripAll :: Cmd -> String
+stripAll (Pen m) = ("pen " ++ penCond m)
+stripAll (Move e1 e2) = ("move " ++ stripExpr e1 ++ stripExpr e2)
+stripAll (Define m v p) = ("define " ++ m ++ " ") -- Unfinished 
+
+stripAll _ = " ERROR: DOES NOT EXIST "
+
+penCond :: Mode -> String
+penCond Up = "up"
+penCond Down = "down"
+
+stripExpr :: Expr -> String
+stripExpr (Ref v) = v
+stripExpr (Lit n) = ("" ++ show n)
+stripExpr (Add e1 e2) = (stripExpr e1) ++ (stripExpr e2)
+
+stripVar :: [Var] -> String
+stripVar [] = []
+-- stripMove :: Expr -> String
+-- stripMove (Ref v)
+-- stripMove ()
+
+
+
+-- ***Temperary Help Line***---
+-- type Prog = [Cmd]
+
+-- data Mode = Up
+--           | Down
+--   deriving (Eq,Show)
+
+-- data Expr = Ref Var
+--           | Lit Num
+--           | Add Expr Expr
+--   deriving (Eq,Show)
+
+-- data Cmd  = Pen Mode
+--           | Move Expr Expr
+--           | Define Macro [Var] Prog
+--           | Call Macro [Expr]
+--   deriving (Eq,Show)
+
+-- prog	::=	ε | cmd; prog
+
+-- mode	::=	down | up
+
+-- expr	::=	var	
+--        | num	
+--        | expr + expr
+
+-- cmd	::=	pen mode	change pen mode
+--        | move ( expr , expr )
+--        | define macro ( var* ) { prog }
+--        | call macro ( expr* )
+
+-- *** TESTS *** --
+-- 
+-- >>> pretty [Pen Up, Move (Ref "x1") (Ref "y1"), Pen Down, Move (Ref "x2") (Ref "y2")]
+-- 
