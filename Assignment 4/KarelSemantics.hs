0ef61d00ef4d728f1d98cbae8ce40f5a8f601e40
Megadrew <morrilan@oregonstate.edu>
2018-02-25 14:31:54 -0800

    Part 1 complete

--- Assignment 4/KarelSemantics.hs
+++ Assignment 4/KarelSemantics.hs
@@ -9,11 +9,18 @@ import KarelState
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
-test = undefined
+test (Not t) w r = if test t w r then False else True
+test (Facing cd) w (p, c, b) = getFacing(p, c, b) == cd 
+test (Clear d) w r = isClear(relativePos d r) w 
+test (Beeper) w (p, _, _) = hasBeeper p w
+test (Empty) w (_, _, b) = if b == 0 then True else False 
+
+
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
+-- stmt Move _ _ _ = let
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
@@ -23,3 +30,10 @@ stmt _ _ _ _ = undefined
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
+
+
+w1 = const Nothing
+w2 = const (Just 0)
+r1 = ((0, 0), North, 0)
+
+t1 = (test (Not(Facing North)) w1 r1)
