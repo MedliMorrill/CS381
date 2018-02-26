40deaa7f86ce80b811e3aa3ef28deb75135c76e8
Megadrew <morrilan@oregonstate.edu>
2018-02-25 16:13:57 -0800

    Finished Part 2

--- Assignment 4/KarelSemantics.hs
+++ Assignment 4/KarelSemantics.hs
@@ -5,6 +5,7 @@ import Data.Function (fix)
 
 import KarelSyntax
 import KarelState
+import KarelExamples
 
 
 -- | Valuation function for Test.
@@ -16,17 +17,33 @@ test (Beeper) w (p, _, _) = hasBeeper p w
 test (Empty) w (_, _, b) = if b == 0 then True else False 
 
 
+-- data Result = OK    World Robot
+--             | Done  Robot
+--             | Error String
 
+-- Move: if position is empty, move there.  Otherwise, error.
+--   World doesnt change, robot position does
+-- PutBeeper: if bag is empty, error. otherwise remove beeper
+-- Turn: turn and return OK, update Robot direction
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown   _ _ r = Done r
--- stmt Move _ _ _ = let
-stmt PickBeeper _ w r = let p = getPos r
-                        in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
-stmt _ _ _ _ = undefined
-    
+stmt Shutdown   _ _ r = Done r 
+stmt Move _ w (p, c, b) = if test (Clear Front) w (p, c, b)
+                            then OK w ((neighbor c p), c, b)
+                            else Error ("Ran into a wall at: " ++ show p)
+stmt PickBeeper _ w r = let q = getPos r
+                        in if hasBeeper q w
+                              then OK (decBeeper q w) (incBag r)
+                              else Error ("No beeper to pick at: " ++ show q)
+stmt PutBeeper _ w r = let q = getPos r
+                       in if isEmpty r
+                             then Error ("No beeper to remove from bag") 
+                             else OK (incBeeper q w) (decBag r)
+stmt (Turn d) _ w (p, c, b) = OK w (p, (cardTurn d c), b) 
+-- stmt (Turn d) _ w r = OK w (setFacing(cardTurn d (getFacing r))r)
+
+
+
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
